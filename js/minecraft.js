15ca9b6d3d0697bcb6b9e933d6e97b2c6046853f
Jessica <jessica.vervaet@student.ucll.be>
2026-08-10 11:48:05 +0200

    8.7 Limited line of data aangepast. 8.8 en 8.9 Sort data. Getest met dummy array. Alles werkt.

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,26 +1,27 @@
-
+/* --- test code dummy array --- */
 // const data = [
 //     {
-//         time: new Date(),
-//         players: 100,
-//         maxplayers: 10000
+//         time: new Date("2026-08-10T08:00:00"),
+//         players: 300,
+//         maxplayers: 9000
 //     },
 //     {
-//         time: new Date(),
+//         time: new Date("2026-08-10T08:05:00"),
 //         players: 200,
 //         maxplayers: 10000
 //     },
 //     {
-//         time: new Date(),
+//         time: new Date("2026-08-10T08:10:00"),
 //         players: 300,
-//         maxplayers: 10000
+//         maxplayers: 10100
 //     },
 //     {
-//         time: new Date(),
+//         time: new Date("2026-08-10T08:03:00"),
 //         players: 150,
 //         maxplayers: 15000
 //     }
 // ];
+/* ---  --- */
 
 const data = [];
 
@@ -35,7 +36,6 @@ const fetchData = async () => {
     };
 
     data.unshift(newDate);
-
 };
 
 const setTable = () => {
@@ -48,11 +48,57 @@ const setTable = () => {
 
 setTable();
 
+const readNumberOfLines = () => {
+    const input = document.querySelector('input[name="numberOfLines"]:checked');
+    return Number(input.value);
+};
+
+const readCheckbox = () => {
+    return document.getElementById("value").checked;
+};
+
+const readTimeSort = () => {
+    return document.getElementById("time-oldest").checked;       
+};
+
 const renderData = (listOfData) => {
+    console.log("renderData krijgt: ", listOfData);
+
     const dataTable = document.getElementById("minecraftTable");
     dataTable.innerHTML = "";
 
-    listOfData.forEach((data) => {
+    const numberOfLines = readNumberOfLines();
+
+    let sortedData = listOfData;
+    
+    if (readCheckbox()) {
+        if (readTimeSort()) {
+            sortedData = sortedData.toSorted((a, b) => {
+                if (a.players !== b.players) {
+                    return b.players - a.players;
+                }
+                return a.time < b.time ? -1 : 1;
+            });
+        } else {
+            sortedData = sortedData.toSorted((a, b) => {
+                if (a.players !== b.players) {
+                    return b.players - a.players;
+                }
+                return a.time > b.time ? -1 : 1;
+            });    
+        }
+    } else {
+
+        if (readTimeSort()) {
+            sortedData = sortedData.toSorted((a, b) => a.time < b.time ? -1 : 1);
+        } else {
+            sortedData = sortedData.toSorted((a, b) => a.time > b.time ? -1 : 1);
+        }
+    }
+
+    const dataToRender = sortedData.slice(0, numberOfLines);
+
+    dataToRender.forEach((data) => {
         const tableRow = createTableRow();
 
         addTableCell({tableRow, value: data.time.toLocaleDateString()});
@@ -64,11 +110,17 @@ const renderData = (listOfData) => {
     });
 };
 
-// renderData(data);
-
 const fetchAndRenderData = async () => {
     await fetchData();
     renderData(data);
 }
 
 setInterval(fetchAndRenderData, 5000);
+
+
+/* --- test code dummy array --- */
+document.querySelectorAll('input[name="time"]').forEach((input) => {
+    input.addEventListener("change", () => {
+        renderData(data);
+    });
+});
