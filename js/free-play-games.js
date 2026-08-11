132e0c66861fab4179090f2551fea7fa4f536b45
Jessica <jessica.vervaet@student.ucll.be>
2026-08-11 13:20:35 +0200

    Alle input en knoppen werken.

--- js/free-play-games.js
+++ js/free-play-games.js
@@ -27,6 +27,23 @@ const createGameCard = (game) => {
     const title = document.createElement("h3");
     title.textContent = game.title;
 
+    const favourite = document.createElement("span");
+    if (game.isFavourite) {
+        favourite.textContent = "★";
+    } else {
+        favourite.textContent = "☆";
+    }       
+
+    favourite.addEventListener("click", () => {
+    game.isFavourite = !game.isFavourite;
+
+    if (game.isFavourite) {
+        favourite.textContent = "★";
+    } else {
+        favourite.textContent = "☆";
+    }
+    });
+
     const genre = document.createElement("p");
     genre.textContent = `Genre: ${game.genre}`;
 
@@ -41,6 +58,7 @@ const createGameCard = (game) => {
 
     card.appendChild(img);
     card.appendChild(title);
+    card.appendChild(favourite);
     card.appendChild(genre);
     card.appendChild(platform);
     card.appendChild(link);
@@ -67,3 +85,145 @@ const fetchAndRenderData = async () => {
 }
 
 fetchAndRenderData();
+
+//---
+
+const searchGames = (listOfGames, searchText) => {
+    return listOfGames.filter((game) =>
+        game.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+};
+
+const searchInput = document.getElementById("search-games");
+
+searchInput.addEventListener("input", () => {
+    filterAndSortGames();
+});
+
+//----
+
+const filterByPlatform = (listOfGames, platform) => {
+    return listOfGames.filter((game) =>
+        game.platform.toLowerCase().includes(platform.toLowerCase())
+    );
+};
+
+const platformInput = document.getElementById("platform");
+
+platformInput.addEventListener("input", () => {
+    filterAndSortGames();
+});
+
+//----
+
+const filterByCategory = (listOfGames, category) => {
+    return listOfGames.filter((game) =>
+        game.genre.toLowerCase().includes(category.toLowerCase())
+    );
+};
+
+const categoryInput = document.getElementById("category");
+
+categoryInput.addEventListener("input", () => {
+    filterAndSortGames();
+});
+
+//----
+
+const sortGames = (listOfGames, sortOption) => {
+    const sortedGames = [...listOfGames];
+
+    if (sortOption === "title-asc") {
+        sortedGames.sort((a, b) => a.title.localeCompare(b.title));
+    }
+
+    if (sortOption === "title-desc") {
+        sortedGames.sort((a, b) => b.title.localeCompare(a.title));
+    }
+
+    if (sortOption === "newest") {
+        sortedGames.sort((a, b) => new Date(b.release_date) - new Date(a.release_date));
+    }
+
+    if (sortOption === "oldest") {
+        sortedGames.sort((a, b) => new Date(a.release_date) - new Date(b.release_date));
+    }
+
+    return sortedGames;
+};
+
+const sortInput = document.getElementById("sort");
+
+sortInput.addEventListener("change", () => {
+    filterAndSortGames();
+});
+
+//---
+
+let showFavourites = false;
+
+const filterAndSortGames = () => {
+    let filteredGames = [...data];
+
+    if (showFavourites) {
+    filteredGames = filterFavouriteGames(filteredGames);
+    }
+
+    const searchText = document.getElementById("search-games").value;
+
+    if (searchText !== "") {
+        filteredGames = searchGames(filteredGames, searchText);
+    }
+
+    const platformInput = document.getElementById("platform").value;
+
+    if (platformInput !== "") {
+        filteredGames = filterByPlatform(filteredGames, platformInput);
+    }
+
+    const categoryInput = document.getElementById("category").value;
+
+    if (categoryInput !== "") {
+        filteredGames = filterByCategory(filteredGames, categoryInput);
+    }
+
+    const sortInput = document.getElementById("sort").value;
+
+    if (sortInput) {
+        filteredGames = sortGames(filteredGames, sortInput);
+    }
+
+    renderData(filteredGames);
+
+};
+
+//---
+
+const isFavourite = (game) => {
+    return game.isFavourite;
+};
+
+const filterFavouriteGames = (listOfGames) => {
+    return listOfGames.filter(isFavourite);
+};
+
+const favouriteButton = document.querySelector("#show-favourite");
+favouriteButton.addEventListener("click", () => {
+    clearSearchInput("search-games");
+    clearSearchInput("platform");
+    clearSearchInput("category");
+
+    showFavourites = true;
+    filterAndSortGames();
+
+});
+
+const allButton = document.querySelector("#show-all");
+allButton.addEventListener("click" , () => {
+    clearSearchInput("search-games");
+    clearSearchInput("platform");
+    clearSearchInput("category");
+    
+    showFavourites = false;
+    renderData(data);
+});
